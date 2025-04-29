da2fe5acfa476874c778a14b9ce9db4da9ca1e99
GIKExe <72767917+GIKExe@users.noreply.github.com>
2025-04-30 02:32:06 +0300

    thread instead of tokio

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,27 @@
+mod data;
+use data::{Packet, Server, Socket};
 
-use std::net::SocketAddr;
+use std::thread;
 
-use tokio::io::AsyncReadExt;
-use tokio::net::{TcpListener, TcpStream};
-
-use rust_mc_proto_tokio::{packet, prelude::*, MCConnTcp, MinecraftConnection, Packet, ProtocolError};
-
-
-#[tokio::main]
-async fn main() {
-	let listener = match TcpListener::bind("127.0.0.1:25565").await {
-		Ok(v) => v,
-		Err(e) => { println!("Не удалось забиндить сервер: {}", e); return; }
+fn main() {
+	let Ok(server) = Server::new("127.0.0.1:25565") else {
+		println!("Не удалось забиндить сервер"); return;
 	};
 
-	while let Ok((stream, addr)) = listener.accept().await {
-		tokio::spawn(handle_connection(stream, addr));
+	loop {
+		let socket = server.accept();
+		thread::spawn(move || { handle_connection(socket); });
 	}
 }
 
-async fn read_handshake_packet(mut packet: Packet) -> Result<(usize, String, u16, usize), ProtocolError> {
-	if packet.id() != 0x00 { return Err(ProtocolError::ReadError)}
-	Ok((
-		packet.read_usize_varint().await?,
-		packet.read_string().await?,
-		packet.read_unsigned_short().await?,
-		packet.read_usize_varint().await?
-	))
-}
+fn handle_connection(socket: Socket) {
+	let Ok(packet) = Packet::read_from(&socket) else {return;};
+	// пакет уже имеет свой размер (size) и данные (data)
+	// надо поместить пакет в очередь, обработать по шаблону и отдать обработчику
 
-async fn handle_connection(stream: TcpStream, addr: SocketAddr) {
-	let mut conn = MinecraftConnection::new(stream);
-
-	let Ok(packet) = conn.read_packet().await else {return;};
-	let Ok((pv, host, port, ns)) = read_handshake_packet(packet).await else {return;};
-
-	if ns == 2 {
-		println!("\nПодключение: {}", addr);
-		println!("Версия протокола: {pv}");
-		println!("Хост: {host}");
-		println!("Порт: {port}");
-	}
-
-	conn.close().await;
+	// fn on_keep_alive(socket: Socket, time: u64) {
+	// 	if time != self.time {
+	// 		socket.close()
+	// 	}
+	// }
 }
