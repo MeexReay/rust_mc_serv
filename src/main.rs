f92f9827c65ca900e3fe696846947f36b13de716
GIKExe <72767917+GIKExe@users.noreply.github.com>
2025-04-29 22:42:55 +0300

    хуйня

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,33 @@
-pub(crate) mod data;
+
+use std::net::SocketAddr;
 
 use tokio::io::AsyncReadExt;
 use tokio::net::{TcpListener, TcpStream};
-use data::VarInt;
+
+use rust_mc_proto_tokio::{packet, prelude::*, MCConnTcp, MinecraftConnection, Packet, ProtocolError};
+
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	// 1. Создаём TCP listener на порту 25565
-	let listener = TcpListener::bind("127.0.0.1:25565").await?;
-	println!("Listening on port 25565...");
+async fn main() {
+	let listener = match TcpListener::bind("127.0.0.1:25565").await {
+		Ok(v) => v,
+		Err(e) => { println!("Не удалось забиндить сервер: {}", e); return; }
+	};
 
-	// 2. Асинхронно принимаем входящие соединения
-	while let Ok((stream, _)) = listener.accept().await {
-		// Для каждого соединения создаём отдельную задачу
-		tokio::spawn(handle_connection(stream));
+	while let Ok((stream, addr)) = listener.accept().await {
+		tokio::spawn(handle_connection(stream, addr));
 	}
-
-	Ok(())
 }
 
-async fn handle_connection(mut stream: TcpStream) {
-	let mut firstByte = [0];
-	let Ok(n) = stream.read(&mut firstByte).await else { return };
-
-	// let mut buffer = [0; 1024];
-
-	// // 3. Читаем данные из потока
-	// while let Ok(n) = stream.read(&mut buffer).await {
-	// 	if n == 0 {
-	// 		// Соединение закрыто
-	// 		break;
-	// 	}
-
-	// 	// 4. Декодируем байты в UTF-8, пропуская ошибки
-	// 	let received = String::from_utf8_lossy(&buffer[..n]);
-	// 	print!("{}", received);
-	// }
-
-	// 5. Соединение автоматически закрывается при выходе из области видимости
+async fn handle_connection(stream: TcpStream, addr: SocketAddr) {
+	let mut conn = MinecraftConnection::new(stream);
+	println!("Подключение: {}", addr);
+	loop {
+		let Ok(mut packet) = conn.read_packet().await else {break;};
+		let Ok(x) = packet.read_bytes(packet.len()).await else {
+			println!("X"); break;
+		};
+		println!("{}", String::from_utf8_lossy(&x));
+	}
+	conn.close().await;
 }
